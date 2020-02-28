f7a8244ce50e94c5d15bbcc972d9e501f8e4e902
xbunny <pitchayarat.p@gmail.com>
2020-02-28 16:55:02 +0700

    28022020

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
+
 using namespace std;
 
 void randData(double *,int,int);
-
 void findRowSum(const double *,double *,int,int);
-
 void showData(double *,int,int);
 
 int main(){
@@ -17,11 +16,34 @@ int main(){
 	double *dPtr = data[0];
 	randData(dPtr,N,M);
 	showData(dPtr,N,M);
-	
 	cout << "---------------------------------------------\n";
-	
 	findRowSum(dPtr,result,N,M);
 	showData(result,N,1);
 }
 
 //Write definition of randData(), showData() and findRowSum()
+
+void randData(double *data ,int N,int M){
+    for(int i=0; i<N*M; i++){
+        *(data+i) = (rand()%101)*0.01;
+        }
+}
+
+void findRowSum(const double *data, double *result, int N, int M){
+    //int a=0;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            *(result+i)+=*(data+i*M+j);
+            //a++;
+            }
+    }
+}
+
+void showData(double *data, int N, int M){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            cout<<setw(8)<<fixed<<setprecision(2)<<*(data+i*M+j)<<" ";
+        }    
+        cout<<"\n";
+    }
+}
--- lab15_2.cpp
+++ lab15_2.cpp
@@ -1,27 +1,49 @@
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
+
 using namespace std;
 
 void randData(double *,int,int);
-
-void findColSum(const double *,double *,int,int);
-
+void findRowSum(const double *,double *,int,int);
 void showData(double *,int,int);
 
 int main(){
 	srand(time(0));
 	const int N = 7, M = 7;
 	double data[N][M] = {};
-	double result[M] = {};
+	double result[N] = {};
 	double *dPtr = data[0];
 	randData(dPtr,N,M);
 	showData(dPtr,N,M);
-	
 	cout << "---------------------------------------------\n";
-	
-	findColSum(dPtr,result,N,M); 
-	showData(result,1,M);
+	findRowSum(dPtr,result,N,M);
+	showData(result,N,1);
 }
 
-//Write definition of randData(), showData() and findColSum()
+//Write definition of randData(), showData() and findRowSum()
+
+void randData(double *data ,int N,int M){
+    for(int i=0; i<N*M; i++){
+        *(data+i) = (rand()%101)*0.01;
+        }
+}
+
+void findRowSum(const double *data, double *result, int N, int M){
+    //int a=0;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            *(result+j)+=*(data+j*M+i);
+            //a++;
+            }
+    }
+}
+
+void showData(double *data, int N, int M){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            cout<<setw(8)<<fixed<<setprecision(2)<<*(data+j*M+i)<<" ";
+        }    
+        cout<<"\n";
+    }
+}
